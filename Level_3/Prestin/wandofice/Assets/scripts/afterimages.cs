f1f70d1b8eef99f20d362f4431443eba410bf270
prestin <presvy1@gmail.com>
2025-11-03 23:22:44 +0530

    wandofice by Prestin

---
+++ Level_3/Prestin/wandofice/Assets/scripts/afterimages.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+public class AfterImage : MonoBehaviour
+{
+ public float fadeSpeed = 5f;
+ private SpriteRenderer sr;
+ private Color color;
+ void Start()
+ {
+ sr = GetComponent<SpriteRenderer>();
+ if (sr == null)
+ {
+ Debug.LogWarning("AfterImage requires a SpriteRenderer.");
+ Destroy(gameObject);
+ return;
+ }
+ color = sr.color;
+ }
+ void Update()
+ {
+ color.a -= fadeSpeed * Time.deltaTime;
+ sr.color = color;
+ if (color.a <= 0f)
+ {
+ Destroy(gameObject);
+ }
+ }
+}
