425135e6e6a88e98cc251de37c40ef4e96166adc
prestin <presvy1@gmail.com>
2025-11-04 03:55:17 +0530

    flashlight by Prestin

---
+++ Level_3/Prestin/flashlight/Assets/scripts/bulletcollision.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Collisiondetection : MonoBehaviour
+{
+   
+    public Sprite damagedSprite;
+
+    
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+       
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+       
+        if (spriteRenderer == null)
+        {
+            Debug.LogError("SpriteRenderer component not found on the GameObject!");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       
+        if (other.CompareTag("Bullet"))
+        {
+            
+            Destroy(other.gameObject);
+
+           
+            if (spriteRenderer != null && damagedSprite != null)
+            {
+                spriteRenderer.sprite = damagedSprite;
+            }
+
+            
+        }
+    }
+}
